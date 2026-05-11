9919d0fa4491d844817b96b4cd423b720e8e8950
lllutanlll <lllutanlll@gmail.com>
2026-05-11 19:14:14 +0900

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,5 +1,13 @@
 module.exports = async function handler(req, res) {
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
 
     const databaseId = process.env.DATABASE_ID;
