0199263ef3044bda983697694b3918d2db07e5d0
lllutanlll <lllutanlll@gmail.com>
2026-05-11 18:42:01 +0900

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,17 +1,16 @@
-export const config = {
-  runtime: 'nodejs',
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
   try {
 
+    const databaseId = process.env.35578bf6192b803582efd15d81ad6745;
+    const token = process.env.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;
+
     const response = await fetch(
-      `https://api.notion.com/v1/databases/${process.env.35578bf6192b803582efd15d81ad6745}/query`,
+      `https://api.notion.com/v1/databases/${databaseId}/query`,
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.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}`,
+          "Authorization": `Bearer ${token}`,
           "Notion-Version": "2022-06-28",
           "Content-Type": "application/json",
         },
@@ -19,24 +18,24 @@ export default async function handler(req, res) {
           filter: {
             property: "現在作業中",
             select: {
-              equals: "作業中",
-            },
-          },
-        }),
+              equals: "作業中"
+            }
+          }
+        })
       }
     );
 
     const data = await response.json();
 
     return res.status(200).json({
-      running: data.results.length > 0,
-      debug: data,
+      running: data.results?.length > 0,
+      debug: data
     });
 
   } catch (e) {
 
-    return res.status(500).json({
-      error: String(e),
+    return res.status(200).json({
+      error: String(e)
     });
 
   }
