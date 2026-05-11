ef2092fc5ea982471536e7f52fb121bb0d1d8e75
lllutanlll <lllutanlll@gmail.com>
2026-05-11 18:16:43 +0900

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,27 +1,3 @@
-export default async function handler(req, res) {
-  try {
-
-    const response = await fetch(
-      `https://api.notion.com/v1/databases/${process.env.35578bf6192b803582efd15d81ad6745}/query`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.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}`,
-          "Notion-Version": "2022-06-28",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    res.status(200).json(data);
-
-  } catch (error) {
-
-    res.status(500).json({
-      error: String(error),
-    });
-
-  }
+export default function handler(req, res) {
+  res.status(200).send("OK");
 }
