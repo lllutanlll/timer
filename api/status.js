834aa7b9cda634f83d3e4ab3fc93786478b76944
lllutanlll <lllutanlll@gmail.com>
2026-05-11 18:01:44 +0900

    Update and rename task.js to status.js

---
+++ api/status.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${process.env.35578bf6192b803582efd15d81ad6745}/query`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.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}`,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        filter: {
+          property: "現在作業中",
+          status: {
+            equals: "作業中",
+          },
+        },
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json({
+    running: data.results.length > 0,
+  });
+}
